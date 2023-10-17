2d6720a605a1f57b449f68a62d0f83eb183454b4
NORZACO <caspermotive4@gmail.com>
2023-10-17 15:45:47 +0200

    Test case for a CreateAPIView subclass

--- store/serialisers/products_serialiser.py
+++ store/serialisers/products_serialiser.py
@@ -1,3 +1,4 @@
+# Description: Products Serialiser
 from rest_framework import serializers
 from store.models.products_models import Product, ShoppingCartItem
 
@@ -16,7 +17,7 @@ class ProductSerializer(serializers.ModelSerializer):
     current_price = serializers.FloatField(read_only=True)
     description = serializers.CharField(min_length=2, max_length=200)
     cart_items = serializers.SerializerMethodField()
-    put_anything = serializers.SerializerMethodField()
+    # put_anything = serializers.SerializerMethodField()
     photo = serializers.ImageField(default=None)
     price = serializers.FloatField(min_value=1.00, max_value=100000)
     price = serializers.DecimalField(
@@ -27,6 +28,8 @@ class ProductSerializer(serializers.ModelSerializer):
     )
 
     sale_start = serializers.DateTimeField(
+        # test
+        required=False,
         input_formats=["%I:%M %p %d %B %Y"],
         format=None,
         allow_null=True,
@@ -34,6 +37,7 @@ class ProductSerializer(serializers.ModelSerializer):
         style={"input_type": "text", "placeholder": "12:01 AM 28 July 2019"},
     )
     sale_end = serializers.DateTimeField(
+         required=False,
         input_formats=["%I:%M %p %d %B %Y"],
         format=None,
         allow_null=True,
@@ -55,7 +59,7 @@ class ProductSerializer(serializers.ModelSerializer):
             "is_on_sale",
             "current_price",
             "cart_items",
-            "put_anything",
+            # "put_anything",
             "photo",
             "warranty",
         )
@@ -66,27 +70,34 @@ class ProductSerializer(serializers.ModelSerializer):
         return CartItemSerializer(items, many=True).data
 
     # get_put_anything
-    def get_put_anything(self, instance):
-        items = ShoppingCartItem.objects.filter(product=instance)
-        return CartItemSerializer(items, many=True).data
+    # def get_put_anything(self, instance):
+    #     items = ShoppingCartItem.objects.filter(product=instance)
+    #     return CartItemSerializer(items, many=True).data
 
     # validate
-    def validate(self, instance, validated_data):
-        if validated_data.get("warranty", None):
-            instance.description += "\n\nWarranty information:\n"
-            instance.description += b"; ".join(
-                validated_data["warranty"].readlines()
-            ).decode()
-        return instance
+    # def validate(self, instance, validated_data):
+    #     if validated_data.get("warranty", None):
+    #         instance.description += "\n\nWarranty information:\n"
+    #         instance.description += b"; ".join(
+    #             validated_data["warranty"].readlines()
+    #         ).decode()
+    #     return instance
+    
+
+    # create
+    def create(self, validated_data):
+        validated_data.pop("warranty")
+        return Product.objects.create(**validated_data)
+    
 
 
     # Validate method with 'attrs' argument
-    # def validate(self, attrs):
-    #     if attrs.get("warranty", None):
-    #         # Modify 'instance' if needed using 'attrs'
-    #         attrs["description"] += "\n\nWarranty information:\n"
-    #         attrs["description"] += b"; ".join(attrs["warranty"].readlines()).decode()
-    #     return attrs
+    def validate(self, attrs):
+        if attrs.get("warranty", None):
+            # Modify 'instance' if needed using 'attrs'
+            attrs["description"] += "\n\nWarranty information:\n"
+            attrs["description"] += b"; ".join(attrs["warranty"].readlines()).decode()
+        return attrs
 
 
 # product state serializer
--- store/tests.py
+++ store/tests.py
@@ -1,4 +1,28 @@
-# from django.test import TestCase
+from store.models.products_models import Product
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+# ProductCreateTestCase
+class ProductCreateTestCase(APITestCase):
+    def test_create_products(self):
+        initial_products_count = Product.objects.count()
+        product_attrs = {
+            "name": "Test product",
+            "description": "Test description",
+            "price": "100.00",
+        }
+        response = self.client.post("/api/v1/products/create", product_attrs)
+        if response.status_code != 201:
+            print(response.data)
 
+        self.assertEqual(
+            Product.objects.count(),
+            initial_products_count + 1,
+        )
+
+        for attr, expected_value in product_attrs.items():
+            self.assertEqual(response.data[attr], expected_value)
+
+        self.assertEqual(response.data["is_on_sale"], False)
+        self.assertEqual(
+            float(response.data["current_price"]), float(product_attrs["price"])
+        )
